191ebe8225e51e4da2bed070e0fd475e5e314026
Javier Parada <javierparada@pm.me>
2025-06-04 12:37:56 +0200

    add sample test

---
+++ tests/utils/unit/sample_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestFindByType(unittest.TestCase):
+    def sample_test(self):
+        """
+        Sample test
+        """
+        data: int = 39
+        data2: int = 39
+        self.assertEqual(data, data2)
